d96af117ddbb1319f1ed2bc2851eb6b5bea688e4
z3to <6gqrbj@posteo.org>
2020-10-21 17:33:27 +0200

    Loop and hover

--- components/ExtendedSSPs.vue
+++ components/ExtendedSSPs.vue
@@ -4,21 +4,21 @@
     <span class="rcp rcp26">Low-end climate change (RCP2.6)</span>
     <span class="col lowcol">Low-level regional collaboration</span>
     <span class="col highcol">High-level regional collaboration</span>
-    <figure class="ssp ssp3" v-tooltip="{ offset: 5, content: 'Regional rivalry is a scenario where Kenya cooperates with neighbouring countries around the common interests and aim to compete against other regional blocs. However, growing nationalist values and intense competition over the scarce resources intensify the rivalry and securitize the relationships between the EAC countries. Access to the technological innovations becomes extremely difficult for Kenya because knowledge sharing practices are securitized, and innovations are strictly patented and expensive.' }">
-      <figcaption class="caption">Extended SSP3: Regional rivalry</figcaption>
-      <img src="img/Regional rivalry-s.jpg" />
-    </figure>
-    <figure class="ssp ssp5" v-tooltip="{ offset: 5, content: 'Fossil-fuelled development is a scenario where regional collaboration in EAC increases with a focus on economic growth and increasing competitiveness in global market. Kenya and neighbours agree on the maximum use of the shared natural resources, but environmental policies including, climate adaptation are not priorities. As a result, climate-induced border conflicts decrease.' }">
-      <figcaption class="caption">Extended SSP5: Fossil fueled development</figcaption>
-      <img src="img/Fossil fueled development_s.jpg" />
-    </figure>
-    <figure class="ssp ssp4" v-tooltip="{ offset: 5, content: 'Inequality is a scenario where regional collaboration in the EAC is fragmented and unstable. While Kenya attempts to maintain collaboration, traditional values upsurge in the region leading the neighbouring countries to enhance militarization and securitized societies. Kenya – like most low-income countries – is struggling to provide the minimum life capacity for the poor majority of the population.' }">
-      <figcaption class="caption">Extended SSP4: Inequality</figcaption>
-      <img src="img/Inequality-s.jpg" />
-    </figure>
-    <figure class="ssp ssp1" v-tooltip="{ offset: 5, content: 'Sustainability is a scenario where regional collaborations towards sustainable development improves within the east African community. Collaborations on natural resource management between Kenya and neighbouring countries ensure preservation and sustainable use of resources and make progress toward resolving cross border conflicts over access to and use of shared resources, especially transboundary water. Clean energy is accessible for a larger share of Kenyan population.' }">
-      <figcaption class="caption">Extended SSP1: Sustainability</figcaption>
-      <img src="img/Sustainability-s.jpg" />
+    <figure
+      v-for="({ ssp, img, tooltip, caption }) in elements"
+      :class="[
+        'ssp',
+        `ssp${ssp}`,
+        {
+          hasActive: active,
+          isActive: active === ssp
+        }
+      ]"
+      @mouseover="() => hoverSSP(ssp)"
+      @mouseleave="leaveSSP"
+      v-tooltip="{ offset: 5, content: tooltip }">
+      <figcaption class="caption">{{ caption }}</figcaption>
+      <img :src="`img/${ssp}.jpg`" />
     </figure>
     <figcaption class="cap figcaption">
       Figure 3. Extended SSPs across climate change and regional collaboration axes. The visualizations were developed by Leti Arts (www.letiarts.com)
@@ -27,7 +27,42 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      active: null
+    }
+  },
+  computed: {
+    elements () {
+      return [{
+        caption: 'Extended SSP3: Regional rivalry',
+        ssp: 3,
+        tooltip: 'Regional rivalry is a scenario where Kenya cooperates with neighbouring countries around the common interests and aim to compete against other regional blocs. However, growing nationalist values and intense competition over the scarce resources intensify the rivalry and securitize the relationships between the EAC countries. Access to the technological innovations becomes extremely difficult for Kenya because knowledge sharing practices are securitized, and innovations are strictly patented and expensive.'
+      }, {
+        caption: 'Extended SSP5: Fossil fueled development',
+        ssp: 5,
+        tooltip: 'Fossil-fuelled development is a scenario where regional collaboration in EAC increases with a focus on economic growth and increasing competitiveness in global market. Kenya and neighbours agree on the maximum use of the shared natural resources, but environmental policies including, climate adaptation are not priorities. As a result, climate-induced border conflicts decrease.'
+      }, {
+        caption: 'Extended SSP4: Inequality',
+        ssp: 4,
+        tooltip: 'Inequality is a scenario where regional collaboration in the EAC is fragmented and unstable. While Kenya attempts to maintain collaboration, traditional values upsurge in the region leading the neighbouring countries to enhance militarization and securitized societies. Kenya – like most low-income countries – is struggling to provide the minimum life capacity for the poor majority of the population.'
+      }, {
+        caption: 'Extended SSP1: Sustainability',
+        ssp: 1,
+        tooltip: 'Sustainability is a scenario where regional collaborations towards sustainable development improves within the east African community. Collaborations on natural resource management between Kenya and neighbouring countries ensure preservation and sustainable use of resources and make progress toward resolving cross border conflicts over access to and use of shared resources, especially transboundary water. Clean energy is accessible for a larger share of Kenyan population.'
+      }]
+    }
+  },
+  methods: {
+    hoverSSP (ssp) {
+      this.active = ssp
+    },
+    leaveSSP () {
+      this.active = null
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -75,6 +110,11 @@ export default {}
     .ssp {
       display: flex;
       flex-direction: column;
+      transition: opacity 0.3s;
+
+      &.hasActive:not(.isActive) {
+        opacity: 0.7;
+      }
 
       &.ssp4, &.ssp1 {
         flex-direction: column-reverse;
